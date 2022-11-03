957caf549ea0dc88bae85b3479013a9a296d35c2
vaarunx <varunrishwandh2001@gmail.com>
2022-11-03 19:54:58 +0530

    flask skelton

--- Project Development Phase/Sprint 1/app.py
+++ Project Development Phase/Sprint 1/app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
