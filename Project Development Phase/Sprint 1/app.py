5d1f847842801627de6f6ff85af571c9c4698bbc
vaarunx <varunrishwandh2001@gmail.com>
2022-11-15 01:25:56 +0530

    registration hashing done

--- Project Development Phase/Sprint 1/app.py
+++ Project Development Phase/Sprint 1/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect
 from flask import render_template
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -19,11 +20,23 @@ def register():
         name = request.form['name']
         email = request.form['email']
         password1 = request.form['password1']
+        dob= request.form['dob']
+
+        bcrypt = Bcrypt()
+        hashed_password = bcrypt.generate_password_hash(request.form['password1']).decode('utf-8')
+
         password2 = request.form['password2']
+        confirmPasswordCheck = bcrypt.check_password_hash(hashed_password, password2)
+        print("Password1: ", password1)
+        print("Password2: " , password2)
+        print("Confirm ", confirmPasswordCheck)
+
+        if(confirmPasswordCheck == 1):
+            print("yes")
         print(name)
         print(email)
-        print(password1)
-        print(password2)
+        print(password2 )
+        print(type(password2))
 
 
     return render_template('register.html', name = "register")
