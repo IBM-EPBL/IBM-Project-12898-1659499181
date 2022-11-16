cb877369b076683c6e113d85db82a8e7ddf76d73
thuhinkhanna <qor6119@gmail.com>
2022-11-16 13:28:10 +0530

    db connection

--- Project Development Phase/Sprint 1/app.py
+++ Project Development Phase/Sprint 1/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, redirect, flash, url_for, session
 from flask import render_template
 from flask_bcrypt import Bcrypt
-from flask_mysqldb import MySQL
 from flask_mail import Mail, Message
 from threading import Thread
 import ibm_db
@@ -13,11 +12,15 @@ mailIDpass = "123thisisit"
 app = Flask(__name__)
 app.secret_key = "super secret key"
 
+cursor = ibm_db.connect("DATABASE=bludb;HOSTNAME=125f9f61-9715-46f9-9399-c8177b21803b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=30426;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=fgq80014;PWD=FfWqq4xxSBCf7CAO","","")
+print(cursor)
 
+'''
 app.config['MYSQL_HOST'] = '125f9f61-9715-46f9-9399-c8177b21803b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud'
-app.config['MYSQL_USER'] = '2116190701229@smartinternz.com'
-app.config['MYSQL_PASSWORD'] = 'Thuhin@6119'
+app.config['MYSQL_USER'] = 'fgq80014'
+app.config['MYSQL_PASSWORD'] = 'FfWqq4xxSBCf7CAO'
 app.config['MYSQL_DB'] = 'bludb'
+'''
 
 #Sending Mail
 
@@ -28,16 +31,12 @@ app.config['MAIL_USERNAME'] = mailID
 app.config['MAIL_PASSWORD'] = mailIDpass
 mail = Mail(app)
 
- 
-mysql = MySQL(app)
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         
-        cursor = ibm_db.connection("DATABASE=bludb;HOSTNAME=125f9f61-9715-46f9-9399-c8177b21803b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=30426;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt")
         bcrypt = Bcrypt()
 
         cursor.execute('''SELECT * FROM user WHERE email = %s''', [email])
@@ -91,11 +90,10 @@ def register():
         bcrypt = Bcrypt()
         hashed_password = bcrypt.generate_password_hash(request.form['password1']).decode('utf-8')
 
-        cursor = mysql.connection.cursor()
-
-
+        sql='''SELECT * FROM user WHERE email = %s'''
+        
         #Check for existing user
-        cursor.execute('''SELECT * FROM user WHERE email = %s''', [email])
+        stmnt = ibm_db.exec_immediate(cursor, sql, [email])
         account = cursor.fetchone()
         if account:
             flash('Account already exists!')
@@ -119,8 +117,6 @@ def forgotPass():
 
     if request.method == 'post':
         email = request.form('email')
-        cursor = mysql.connection.cursor()
-
 
         #Check for existing user
         cursor.execute('''SELECT * FROM user WHERE email = %s''', [email])
@@ -133,11 +129,4 @@ def forgotPass():
             msg.sender = mailID
             # msg.html = render_template('reset_email.html', user=account, token=token)
 
-
-
     return render_template('forgotPassword.html')
-
-
-
-
-
